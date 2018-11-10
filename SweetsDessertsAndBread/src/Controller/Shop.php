d7433d33499da87aaa5c4ee378972846d27660ad
Marc <marcmde95@gmail.com>
2018-11-10 11:02:51 +0100

    Twig...Templateeees

--- SweetsDessertsAndBread/src/Controller/Shop.php
+++ SweetsDessertsAndBread/src/Controller/Shop.php
@@ -7,17 +7,25 @@
  */
 
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class Shop
+class Shop extends AbstractController
 {
     /**
      * @Route("/shop/")
      */
     public function Index()
     {
-        return new Response("OMG! It works!");
+        //return new Response("OMG! It works!");
+
+        $exempleArr = ["Hola1", "Hola2", "Hola3"];
+
+        return $this->render('shop/index.html.twig', [
+            'title' => "SD&B",
+            'arr' => $exempleArr
+        ]);
     }
 
 }
